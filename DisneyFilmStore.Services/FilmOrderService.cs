30910b4d9eed5a410b80f0cbe03de5bbf96653df
Dean Collier <daccollier@gmail.com>
2021-09-02 16:42:41 -0500

    added FilmOrder functinoality

--- DisneyFilmStore.Services/FilmOrderService.cs
+++ DisneyFilmStore.Services/FilmOrderService.cs
@@ -1,5 +1,6 @@
 ﻿using DisneyFilmStore.Data;
 using DisneyFilmStore.Models.FilmOrderModels;
+using DisneyFilmStore.Models.OrderModels;
 using System;
 using System.Collections.Generic;
 using System.Data.Entity;
@@ -101,6 +102,52 @@ namespace DisneyFilmStore.Services
 
                 return await context.SaveChangesAsync() == 1;
             }
+        }
+
+        public async Task<int> UpdateFilmOrderFromOrderUpdateAsync(OrderEdit model)
+        {
+            int changesCount = 0;
+            using (var context = new ApplicationDbContext())
+            {
+                var query =
+                    context
+                        .FilmOrders
+                        .Where(fo => fo.OrderId == model.OrderId && fo.UserId == _userId);
+
+                var currentFilmOrders = await query.ToArrayAsync();
+
+                // old films: 1 2 3
+                // updated films: 3 7 8 9
+
+                List<int> currentFilmIds = new List<int>(); // list of current film Ids for order
+                foreach (var filmOrder in currentFilmOrders)
+                {
+                    currentFilmIds.Add(filmOrder.FilmId);
+                }
+
+                foreach (var filmId in currentFilmIds) // deleting current films no longer in the edited order
+                {
+                    if (!(model.FilmIds.Contains(filmId)))
+                    {
+                        await DeleteFilmOrderByIdAsync(filmId);
+                        changesCount++;
+                    }
+                }
+                foreach (var filmId in model.FilmIds) // adding films that were not previously in the order
+                {
+                    if (!(currentFilmIds.Contains(filmId)))
+                    {
+                        await CreateFilmOrderAsync(
+                            new FilmOrderCreate
+                            {
+                                FilmId = filmId,
+                                OrderId = model.OrderId
+                            });
+                        changesCount++;
+                    }
+                }
+            }
+            return changesCount;
         }
 
         // DELETE
